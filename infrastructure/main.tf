c137d6b523c822c2d5c54eb80367893e7e6fbf73
Sudhanshu <subgor8126@gmail.com>
2025-08-08 11:53:53 +0530

    cicd: dummy thumbprints

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -179,12 +179,7 @@ resource "aws_iam_openid_connect_provider" "github" {
     "sts.amazonaws.com",
   ]
   
-  thumbprint_list = [
-    "6938fd4d98bab03faadb97b34396831e3780aea1",
-    "1c58a3a8518e8759bf075b76b750d4f2df264fcd",
-    "a031c46782e6e6c662c2c87c76da9aa62ccabd8e",
-    "ffcf8b2e035aed89a2008c47b164b14d84723ee0"
-  ]
+  thumbprint_list = ["0000000000000000000000000000000000000000"]
   
   tags = {
     Name = "github-actions-oidc"
